71246a5e0fbf698a952f3200e8d07b8e8e69308f
Ali Madan <alimadan4321@gmail.com>
2025-07-09 11:41:24 +0300

    add duck

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,24 @@
 function init(){
     //All our code goes here
     const gridElem = document.querySelector('.grid')
+
+    const cells = []
+
+    const gridWidth = 10
+
+    const numberCells = gridWidth * gridWidth
+
     function createGrid(){
         //For every cell create a div
         //Append this to our grid
-        for (let i = 0; i < 100; i++){
+        for (let i = 0; i < numberCells; i++){
             const cell = document.createElement('div')
+            cell.classList.add('duck')
             cell.textContent = i
+            cells.push(cell)
             gridElem.appendChild(cell)
         }
+        console.log(cells)
     }
 
     createGrid()
