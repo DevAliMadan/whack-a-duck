0064619beed51b7a88205552081556c6fc378be9
Ali Madan <alimadan4321@gmail.com>
2025-07-09 13:33:02 +0300

    randomize the duck postin

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,31 @@ function init(){
     const gridWidth = 10
 
     const numberCells = gridWidth * gridWidth
+     
+    let duckPositon = [90]
 
+
+    function play(){
+        setInterval(() => {
+            removeDuck()
+            duckPositon = Math.floor(Math.random()* numberCells)
+            addDuck()
+        }, 2000);
+    }
+    function addDuck(){
+        cells[duckPositon].classList.add('duck')
+    }
+
+    function removeDuck() {
+        cells[duckPositon].classList.remove('duck')
+    }
+    
     function createGrid(){
         //For every cell create a div
         //Append this to our grid
         for (let i = 0; i < numberCells; i++){
             const cell = document.createElement('div')
-            cell.classList.add('duck')
+            // cell.classList.add('duck')
             cell.textContent = i
             cells.push(cell)
             gridElem.appendChild(cell)
@@ -22,8 +40,8 @@ function init(){
     }
 
     createGrid()
-
-
+    addDuck()
+    play()
 
 
 
