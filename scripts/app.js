bf4a4ab6e70233be7ccb023aaa93c8d3788a1f53
Ali Madan <alimadan4321@gmail.com>
2025-07-09 13:56:41 +0300

    add score

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 function init(){
     //All our code goes here
     const gridElem = document.querySelector('.grid')
+    const scoreElem = document.querySelector('#score-display')
 
     const cells = []
 
@@ -8,12 +9,15 @@ function init(){
 
     const numberCells = gridWidth * gridWidth
      
-    let duckPositon = [90]
+    let duckPositon = [1]
+
+    let score = 0
 
 
     function play(){
         setInterval(() => {
             removeDuck()
+            //Ramdomizer
             duckPositon = Math.floor(Math.random()* numberCells)
             addDuck()
         }, 2000);
@@ -25,7 +29,14 @@ function init(){
     function removeDuck() {
         cells[duckPositon].classList.remove('duck')
     }
-    
+    function handleClick(event){
+        console.log("runs")
+        if (event.target.classList.contains('duck')){
+            score += 10
+            scoreElem.textContent = `Your score is ${score}`
+            console.log(score)
+        }
+    }
     function createGrid(){
         //For every cell create a div
         //Append this to our grid
@@ -33,6 +44,7 @@ function init(){
             const cell = document.createElement('div')
             // cell.classList.add('duck')
             cell.textContent = i
+            cell.addEventListener('click', handleClick)
             cells.push(cell)
             gridElem.appendChild(cell)
         }
